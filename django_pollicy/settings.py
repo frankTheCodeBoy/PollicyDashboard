f92ba6686491425fc8478253070b800c7b683627
frankTheCodeBoy <olumfrank48@gmail.com>
2023-03-23 14:51:19 +0300

    Main page bg

--- django_pollicy/settings.py
+++ django_pollicy/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = path.join(BASE_DIR, 'static')
+STATIC_DIR = path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR,]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = 'media/'
 
