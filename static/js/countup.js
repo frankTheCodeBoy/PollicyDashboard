b26e546b08ae1b65b1b3954dcebcc201f3ed94d1
frankTheCodeBoy <olumfrank48@gmail.com>
2023-04-14 22:30:48 +0300

    state dynamic

--- static/js/countup.js
+++ static/js/countup.js
@@ -53,3 +53,55 @@ valDisplays.forEach((valDisplay) => {
 
 
 
+function myCountUp() {
+  // for first data-group
+let valueDisplays = document.querySelectorAll(".cl-num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+// for second data-group
+let newValueDisplays = document.querySelectorAll(".cl-newnum");
+let newInterval = 4000;
+
+newValueDisplays.forEach((newValueDisplay) => {
+  let startValue = 69200;
+  let endValue = parseInt(newValueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(newInterval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    newValueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+//for third data-group
+let valDisplays = document.querySelectorAll(".cl-mynewnum");
+let valInterval = 4000;
+
+valDisplays.forEach((valDisplay) => {
+  let startValue = 297700;
+  let endValue = parseInt(valDisplay.getAttribute("data-val"));
+  let duration = Math.floor(valInterval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+  });
+;}
