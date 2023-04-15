cba1af847da051271c7a9f2c894c2ddfbcf91a9a
frankTheCodeBoy <olumfrank48@gmail.com>
2023-04-15 20:58:45 +0300

    script ml update

--- machine_script.py
+++ machine_script.py
@@ -10,32 +10,33 @@ def machine_fun(text):
         'petty', 'imbecile', 'idle', 'cabbage', 'head', 'bastard',
         'stubborn', 'bugger', 'idiot', 'idiots', 'dummy', 'dumb',
         'kwenda', 'mchafu', 'potea', 'shenzi', 'shetani', 'mjinga',
-        'kumbafu', 'pepo', 'chafu', 'nyamaza', 'kelele', 'ngombe',
+        'kumbafu', 'pepo', 'chafu', 'nyamaza', 'kelele', 'ngombe', 'mbwa',
+        'takataka',
     ]
 
     sexual_violence_list = [
         'sexy', 'fuck', 'fucker', 'rape', 'female',
         'kill', 'pretty', 'dick', 'maggot', 'feeble',
         'tits', 'boobs', 'fucked', 'ass', 'arsehole', 'mama',
-        'kuma', 'malaya', 'shoga', 
+        'kuma', 'malaya', 'shoga', 'tombwa', 'katombwe',
     ]
 
     shame_list = [
         'ugly', 'fat', 'bald', 'flabby', 'chubby',
         'little', 'small', 'smelly', 'old', 'midget',
         'mfupi', 'hauna', 'fukara', 'bandia', 'senye',
-        'matako', 'masikini',
+        'matako', 'masikini', 'kadogo', 'mdogo',
     ]
 
     troll_list = [
-        'haha', 'hahaha', 'small', 'insignificant',
+        'haha', 'hahaha', 'hahahaha', 'small', 'insignificant',
         'useless', 'hell', 'imbecile', 'funny',
         'nobody', 'no', 'one', 'didnt', 'lovely', 'stop',
-        'liar', 'lying', 'false', 'hard', 'harder',
-        'seeker', 'cheap', 'shot', 'propaganda',
+        'liar', 'lying', 'false', 'hard', 'harder', 'waster',
+        'seeker', 'cheap', 'shot', 'propaganda', 'out',
         'waste', 'wasting', 'unfriendly', 'tryhard',
         'hopeless', 'braggart', 'wishful', 'ashamed', 'shame',
-        'religious', 'ungodly', 'hiding', 'hide', 'doom',
+        'religious', 'ungodly', 'hiding', 'hide', 'doom', 'losing',
         'clout', 'chaser', 'hopeless', 'leave', 'hater', 'loser',
         'go', 'away', 'kick', 'kicked', 'hahahaha', 'hahaha',
         'stop', 'doll', 'toka', 'nenda', 'ondoka', 'wewe',
