509d3bd7e78ab3e85a7d38fe2b7e563310e023ab
frankTheCodeBoy <olumfrank48@gmail.com>
2023-04-21 17:08:30 +0300

    changes to js countup file

--- static/js/countup.js
+++ static/js/countup.js
@@ -4,7 +4,7 @@ let valueDisplays = document.querySelectorAll(".num");
 let interval = 4000;
 
 valueDisplays.forEach((valueDisplay) => {
-  let startValue = 0;
+  let startValue = 10;
   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
   let duration = Math.floor(interval / endValue);
   let counter = setInterval(function () {
