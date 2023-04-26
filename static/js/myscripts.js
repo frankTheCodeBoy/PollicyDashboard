362c494cc9a867e7204a9f538fdac784fc37c7b2
frankTheCodeBoy <olumfrank48@gmail.com>
2023-04-26 20:01:16 +0300

    timed slideshow changes

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -81,6 +81,22 @@ $(document).ready(function() {
       }
     )
 
+      //for the slideshow
+      var myIndex = 0;
+        carousel();
+
+        function carousel() {
+        var i;
+        var x = document.getElementsByClassName("myImgSlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 4000); // Change image every 4 seconds
+        }
+
 })
 
 
