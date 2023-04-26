e3bc55decedf76e9242c10519a9f3e0614509948
frankTheCodeBoy <olumfrank48@gmail.com>
2023-04-26 20:16:15 +0300

    optimization

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
       function(){
           $(this).css('background-color',elColor);
       }
-    )
+    );
 
     var varCol = $('.mycustom-hover').css('background-color');
     $('.mycustom-hover').hover(
@@ -69,7 +69,7 @@ $(document).ready(function() {
       function(){
           $(this).css('background-color',varCol);
       }
-    )
+    );
 
     var els = $('.tr-hover').css('background-color');
     $('.tr-hover').hover(
@@ -79,23 +79,7 @@ $(document).ready(function() {
       function(){
           $(this).css('background-color',els);
       }
-    )
-
-      //for the slideshow
-      var myIndex = 0;
-        carousel();
-
-        function carousel() {
-        var i;
-        var x = document.getElementsByClassName("myImgSlides");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";  
-        }
-        myIndex++;
-        if (myIndex > x.length) {myIndex = 1}    
-        x[myIndex-1].style.display = "block";  
-        setTimeout(carousel, 4000); // Change image every 4 seconds
-        }
+    );
 
 })
 
